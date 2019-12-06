6b85f8a01b01d992cfa647227ae1c66e0cf654de
Jason Aviles <sandman2k18@outlook.com>
2019-12-06 13:39:16 -0500

    mvp

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -8,12 +8,35 @@ from hashtables import (HashTable,
 
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
+    weight_list = []
+    tries = set()
+    for weight in weights:
+        hash_table_insert(ht, weight, weights.index(weight))
 
-    """
-    YOUR CODE HERE
-    """
+        weight_list.append(weight)
 
-    return None
+    counter = 0
+    for weight in weight_list:
+        difference = limit - weight
+        if difference in weight_list and difference != 0:
+            counter += 1
+
+            if hash_table_retrieve(ht, difference) is not None and counter < length:
+                if hash_table_retrieve(ht, weight) > hash_table_retrieve(ht, difference):
+                    answer = [hash_table_retrieve(ht, weight), hash_table_retrieve(ht, difference)]
+
+                elif hash_table_retrieve(ht, weight) < hash_table_retrieve(ht, difference):
+
+                    answer = [hash_table_retrieve(ht, difference), hash_table_retrieve(ht, weight)]
+
+                elif hash_table_retrieve(ht, weight) == hash_table_retrieve(ht, difference):
+                    answer = [hash_table_retrieve(ht, difference) + 1, hash_table_retrieve(ht, weight)]
+
+                return answer
+
+            if counter >= length:
+
+                return None
 
 
 def print_answer(answer):
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -15,9 +15,57 @@ class Ticket:
 def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
+    ticket_list = []
 
-    """
-    YOUR CODE HERE
-    """
+    for ticket in tickets:
+        ticket_list.append(ticket)
+
+    length = 0
+    recent = None
+    while length < len(tickets):
+        for ticket in ticket_list:
+            if ticket.source == "NONE":
+                route.append(ticket.destination)
+                recent = ticket.destination
+                ticket_list.remove(ticket)
+                length +=1
+            else:
+                recent = ticket.destination
+                next_link = hash_table_retrieve(hashtable, recent.destination)
+                route.append(next_link.destination)
+                length +=1
+
+
+
+
+    print(type(route))
+
+  
+
+    length = 0
+    while length < len(ticket_list):
+        for ticket in ticket_list:
+            if ticket.source == "NONE":
+                route.insert(1, ticket.destination)
+                ticket_list.remove(ticket)
+                length += 1
+
+            else:
+                if ticket.source != "NONE":
+                    previous = hash_table_retrieve(hashtable, ticket.destination)
+                    #print(previous)
+                    route.append(ticket.destination)
+                    route.append(ticket.source)
+                    ticket_list.remove(ticket)
+                    length += 1
+
+                else:
+                    print("IS NONE")
+                    #route.append(ticket.source)
+                    ticket_list.remove(ticket)
+                    length += 1
+
+    print(route)
+    
 
     return route
